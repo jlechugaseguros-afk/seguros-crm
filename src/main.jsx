d9870b45693b590b880f0f04c451c953de39022e
jlechugaseguros-afk <jlechuga.seguros@gmail.com>
2026-09-11 07:24:32 +0000

    Corrige ruteo de la liga publica

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import './index.css'
 import './storage.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
+import TarjetaPublica from './TarjetaPublica.jsx'
+
+const path = window.location.pathname
+const publicMatch = path.match(/^\/t\/([^/]+)\/?$/)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
+    {publicMatch ? (
+      <TarjetaPublica agentId={publicMatch[1]} />
+    ) : (
+      <AuthGate>
+        <App />
+      </AuthGate>
+    )}
   </StrictMode>,
 )
